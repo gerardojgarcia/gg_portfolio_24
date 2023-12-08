c62e99be764351354701ad57e01f46500ee73a32
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-12-08 11:00:00 -0800

    updated blog pages; macbook

--- pages/blog/html-tags-for-beginners.vue
+++ pages/blog/html-tags-for-beginners.vue
@@ -10,9 +10,9 @@
 
   <h2>Chapter 1. The Building Blocks  - Introduction to HTML Tags</h2>
 
-  <div class="flex items-center relative text-gray-200 bg-gray-800 dark:bg-token-surface-primary px-4 py-2 text-xs font-sans justify-between rounded-t-md">
+  <div class="flex items-center relative  text-gray-200 bg-gray-800 dark:bg-token-surface-primary px-4 py-2 text-xs font-sans justify-between rounded-t-md">
     <span>html</span><button class="flex gap-1 items-center"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-sm"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z" fill="currentColor"></path></svg>Copy code</button></div><div class="p-4 overflow-y-auto">
-  <code class="!whitespace-pre hljs language-html"><span class="hljs-meta">&lt;!DOCTYPE <span class="hljs-keyword">html</span>&gt;</span>
+  <code class="!whitespace-pre hljs language-html w-60 "><span class="hljs-meta">&lt;!DOCTYPE <span class="hljs-keyword">html</span>&gt;</span>
   <span class="hljs-tag">&lt;<span class="hljs-name">html</span>&gt;</span>
   <span class="hljs-tag">&lt;<span class="hljs-name">head</span>&gt;</span>
   <span class="hljs-tag">&lt;<span class="hljs-name">title</span>&gt;</span>Your Page Title<span class="hljs-tag">&lt;/<span class="hljs-name">title</span>&gt;</span>
@@ -28,7 +28,7 @@
 
 <style scoped>
 
-code hljs, code[class*="language-"], pre[class*="language-"] {
+code .hljs, code[class*="language-"], pre[class*="language-"] {
   word-wrap: normal;
   background: none;
   color: #fff;
